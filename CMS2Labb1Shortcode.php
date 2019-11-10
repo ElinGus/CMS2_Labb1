fcb1f1247a7054ccfc141690704f4ffa3f43db96
ElinGus <48243380+ElinGus@users.noreply.github.com>
2019-11-10 22:43:05 +0100

    Add files via upload

--- CMS2Labb1Shortcode.php
+++ CMS2Labb1Shortcode.php
@@ -6,31 +6,38 @@ Author: Elin Gustafsson
 Version: 1.0
 */
 
-// Shortcode [button]
+
+// / Funktion för shortcode [button]
 function button_shortcode( $atts = '' ) {
   
-  //wp_enqueue_style('shortcode-button-css');
-  
+  // Attribut för shortcoden
 	$value = shortcode_atts( array(
     'text' => 'Knapp',
     'background' => '#aaa',
-    'ulr' => '',
+    'url' => '',
     'width' => '',
     'style' => '',
 	), $atts );
 
+// Variabler för attributen
   $bg_color = $value['background'];
   $btn_url = $value['url'];
   $btn_width = $value['width'];
   $btn_style = $value['style'];
   
-	return '<button style="background-color:' . $bg_color . '; url:'. $btn_url .'; width:'. $btn_width .'; style:'. $btn_style .';" class="button"><a href="https://www.wordpress.org">' . $value['text'] . '</a></button>';
+  // Shortcoden retunerar en knapp med de attribut den fått 
+	return '<button style="background-color:' . $bg_color . '; '. $btn_style .'; width:' . $btn_width . ';" class="shortcodebutton"><a href="'. $btn_url .'">' . $value['text'] . '</a></button>';
+  
 }
 
-// function styles() {
-//     wp_register_style('shortcode-button-css', plugin_dir_url(__FILE__).'CMS2Labb1Shortcode.css');
-// }  
+// Läser in CSS-filen 
+function register_button_plugin_styles() {
+  wp_register_style('shortcode-button-css', plugin_dir_url(__FILE__).'CMS2Labb1Shortcode.css');
+  wp_enqueue_style('shortcode-button-css');
+}
+add_action( 'wp_enqueue_scripts', 'register_button_plugin_styles' );
 
+// Läser in shortcodens funktion så den kan visas i fontend 
 add_shortcode( 'button', 'button_shortcode' );
 
 
