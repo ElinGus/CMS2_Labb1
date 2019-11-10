beb3786751c734b251293363980c6caa7992932e
ElinGus <48243380+ElinGus@users.noreply.github.com>
2019-11-10 22:42:42 +0100

    Add files via upload

--- CMS2Labb1ContactForm.php
+++ CMS2Labb1ContactForm.php
@@ -7,19 +7,22 @@ Author: Elin Gustafsson
 Version: 1.0
 */
 
-//[contact]
+// Funktion för shortcoden [contact]
 function contact_shortcode( $atts, $content = null ){
   
+  // Attribut för shortcoden
   $value = shortcode_atts( array(
     'receiver' => 'myemail@mail.com',
-    'placeholder' => 'Skriv ditt medelande här',
+    'placeholder' => 'Skriv ditt meddelande här',
     'success-text' => 'Ditt meddelande har skickats!',
 	), $atts );
   
+  // Gör variabler för attributen
   $form_receiver = $value['receiver'];
   $form_placeholder = $value['placeholder'];
   $form_success = $value['success-text'];
   
+  // Om man skickar ett meddelande postas det vidare via wp-mail 
   if (isset($_POST['submit'])) {
     $to = $form_receiver;
     $subject = $_POST["subject"];
@@ -30,24 +33,22 @@ function contact_shortcode( $atts, $content = null ){
       return $form_success;
   }
   else {
-    return ''. $content .'<form method="post" class="contact_form">
-      Ämne: <input type="text" name="subject">
-      Message: <textarea name="message">'. $form_placeholder .' </textarea>
+    // Shortcoden retunerar ett kontaktformulär 
+    return ''. $content .'<form method="post" class="contact_form"> 
+      <h5>Ämne:</h5><input type="text" name="subject">
+      <h5>Message:</h5><textarea name="message">'. $form_placeholder .' </textarea>
       <input type="submit" name="submit" value="Skicka"></form>';
-  }
-  
-
-    // $multiple_to_recipients = array(
-    //     'somebody@example.com',
-    //     'somebodyelse@example.com'
-    // );
-    // $subject = 'The subject';
-    // $message = 'The message';
-    // 
-    // wp_mail( $multiple_to_recipients, $subject, $message );
-
+  }  
 }
-  
+
+// Läser in CSS-filen 
+function register_form_plugin_styles() {
+  wp_register_style('shortcode-form-css', plugin_dir_url(__FILE__).'CMS2Labb1ContactForm.css');
+  wp_enqueue_style('shortcode-form-css');
+}
+add_action( 'wp_enqueue_scripts', 'register_form_plugin_styles' );
+
+// Läser in shortcodens funktion så den kan visas i fontend  
 add_shortcode( 'contact', 'contact_shortcode' );
 
 
